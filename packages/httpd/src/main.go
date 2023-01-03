8f411df2a3ab7ee18879b3bcdad6b0e551856363
David Alsh <alshdavid@gmail.com>
2023-01-04 09:06:41 +1100

    figure out this app issue

--- packages/httpd/src/main.go
+++ packages/httpd/src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -15,18 +16,24 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(r)
+		b, _ := json.Marshal(r.URL)
+		fmt.Println("test")
+		fmt.Println(r.Host)
+		fmt.Println(string(b))
 		io.WriteString(w, "Hello Test!")
 	})
 
 	r.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(r)
+		b, _ := json.Marshal(r.URL)
+		fmt.Println("fallback")
+		fmt.Println(r.Host)
+		fmt.Println(string(b))
 		io.WriteString(w, "Catch all")
 	})
 
 	if os.Getenv("HTTPD") == "true" {
 		http.ListenAndServe("0.0.0.0:3000", r)
 	} else {
-		lambda.Start(httpadapter.New(r).ProxyWithContext)
+		lambda.Start(httpadapter.New(r).Proxy)
 	}
 }
